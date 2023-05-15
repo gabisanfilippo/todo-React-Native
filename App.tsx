aae03dd3af1ad5ec7ac348eb6bc8e518ca88130f
Gabrielle Coelho Sanfilippo <gabrielle.sanfilippo@veredaeducacao.com>
2023-05-15 11:33:51 -0300

    test: styled-components

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
 
 export default function App() {
+  console.log('olaaa')
+  const Teste = styled.View`
+    background-color: red;
+    width: 100%;
+    height: 100%;
+  `
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <Teste >
+      <Text>Hello World!!</Text>
       <StatusBar style="auto" />
-    </View>
+    </Teste>
   );
 }
 
