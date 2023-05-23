93009587320fd58fe3c67b593393bcefde64dbad
Gabrielle Coelho Sanfilippo <gabriellecoelhosanfilippo@gmail.com>
2023-05-23 08:11:07 -0300

    feat: componentization of home

--- App.tsx
+++ App.tsx
@@ -1,27 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import styled from 'styled-components/native';
+import { StatusBar } from "expo-status-bar";
+import { Home } from "./src/pages/Home";
 
 export default function App() {
-  console.log('olaaa')
-  const Teste = styled.View`
-    background-color: red;
-    width: 100%;
-    height: 100%;
-  `
   return (
-    <Teste >
-      <Text>Hello World!!</Text>
-      <StatusBar style="auto" />
-    </Teste>
+    <>
+      <StatusBar animated={true} style="light" />
+      <Home />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
